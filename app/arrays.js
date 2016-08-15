12d390b04faf03b03b7df6b3b37639f2acdc590f
Keane Johnson <keanejohnson2013@gmail.com>
2016-08-15 14:05:37 -0400

    completed first half of array exercises

--- app/arrays.js
+++ app/arrays.js
@@ -3,31 +3,42 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   removeWithoutCopy : function(arr, item) {
-
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] == item){
+        arr.splice(i, item)
+      };
+    };
+    return arr;
   },
 
   append : function(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate : function(arr) {
-
+    arr.splice(-1, 1);
+    return arr;
   },
 
   prepend : function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail : function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat : function(arr1, arr2) {
-
+    var arr3 = arr1.concat(arr2)
+    return arr3;
   },
 
   insert : function(arr, item, index) {
-
+    var newArr = arr.splice(index, 0, item);
+    return newArr;
   },
 
   count : function(arr, item) {
